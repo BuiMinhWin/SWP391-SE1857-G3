c88aca77be20aa5d94a30843384946cfb97092bb
Nguyễn Anh Quân <144054352+BlueBerryD42@users.noreply.github.com>
2024-09-25 01:57:17 +0700

    App.js changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Greet from "./Component/greet"; //Named export
+import Welcome from "./Component/welcome";
+import Hello from "./Component/Hello";
+import Message from "./Component/Message";
+import Count from "./Component/Counter";
+import FunctionClick from "./Component/FunctionClick";
+import ClassClick from "./Component/ClassClick";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ClassClick />
+        <FunctionClick />
+
+        {/* <Count /> */}
+        {/* <Message /> */}
+
+        {/* <Greet name="Quan" classRoom="SE1857">
+          <p>This is children prop</p>
+        </Greet>
+        <Greet name="Thang" classRoom="SE1858"> 
+          <button>COCC</button>
+        </Greet>
+        <Greet name="Phong" classRoom="SE1859"/> */}
+
+        {/* <Welcome name="Quan" classRoom="SE1857" />
+        <Welcome name="Thang" classRoom="SE1858"/>
+        <Welcome name="Phong" classRoom="SE1859"/> */}
+        {/* <Hello /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
